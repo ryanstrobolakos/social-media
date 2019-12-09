393e061eb2b3a20dd7de2bb0ad4a094fdc2fd2ff
Ryan Strobolakos <ryanstrobolakos@Ryans-MacBook-Air.local>
2019-12-09 11:12:41 -0500

    adding Tweet class

---
+++ src/main/java/com/techtalentsouth/SocialMedia/model/Tweet.java
@@ -0,0 +1,44 @@
+package com.techtalentsouth.SocialMedia.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Tweet {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "tweet_id")
+	private Long id;
+		
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "user_id")
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	private User user;
+		
+	private String message;
+		
+	@CreationTimestamp 
+	private Date createdAt;
+
+}
