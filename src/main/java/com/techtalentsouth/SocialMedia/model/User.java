7da99f8d0bbfb85e47102fdf7ae7c4291ae47d9f
Ryan Strobolakos <ryanstrobolakos@Ryans-MacBook-Air.local>
2019-12-11 09:10:07 -0500

    adding Many to Many relationship for followers and following in User class

--- src/main/java/com/techtalentsouth/SocialMedia/model/User.java
+++ src/main/java/com/techtalentsouth/SocialMedia/model/User.java
@@ -1,6 +1,7 @@
 package com.techtalentsouth.SocialMedia.model;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -62,6 +63,14 @@ public class User {
 	@JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), 
 	    inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles;
+	
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(name = "user_follower", joinColumns = @JoinColumn(name = "user_id"), 
+	    inverseJoinColumns = @JoinColumn(name = "follower_id"))
+	private List<User> followers;
+	
+	@ManyToMany(mappedBy="followers")
+	private List<User> following;
 }
 
 
