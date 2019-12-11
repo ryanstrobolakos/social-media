3851bc4b476a7cc8b703fb95a621c9bd33bc16fb
Ryan Strobolakos <ryanstrobolakos@Ryans-MacBook-Air.local>
2019-12-11 09:45:04 -0500

    adding import statements and findByPhrase method to tag repository due to incorrect save

--- src/main/java/com/techtalentsouth/SocialMedia/repository/TagRepository.java
+++ src/main/java/com/techtalentsouth/SocialMedia/repository/TagRepository.java
@@ -1,5 +1,12 @@
 package com.techtalentsouth.SocialMedia.repository;
 
-public interface TagRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.techtalentsouth.SocialMedia.model.Tag;
+
+@Repository
+public interface TagRepository extends CrudRepository<Tag, Long> {
+	Tag findByPhrase(String phrase);
 
 }
