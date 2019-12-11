ba4f4741fc8bc3052e536dd09f8f5cf7c8c2d6d3
Ryan Strobolakos <ryanstrobolakos@Ryans-MacBook-Air.local>
2019-12-11 08:02:32 -0500

    adding get feed, get new tweet, and submit tweet methods to Tweet Controller

--- src/main/java/com/techtalentsouth/SocialMedia/controller/TweetController.java
+++ src/main/java/com/techtalentsouth/SocialMedia/controller/TweetController.java
@@ -1,8 +1,18 @@
 package com.techtalentsouth.SocialMedia.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.techtalentsouth.SocialMedia.model.Tweet;
+import com.techtalentsouth.SocialMedia.model.User;
 import com.techtalentsouth.SocialMedia.service.TweetService;
 import com.techtalentsouth.SocialMedia.service.UserService;
 
@@ -13,5 +23,30 @@ public class TweetController {
 	
     @Autowired
     private TweetService tweetService;
+    
+    @GetMapping(value= {"/tweets", "/"})
+    public String getFeed(Model model){
+        List<Tweet> tweets = tweetService.findAll();
+        model.addAttribute("tweetList", tweets);
+        return "feed";
+    }
+    
+    @GetMapping(value = "/tweets/new")
+    public String getTweetForm(Model model) {
+        model.addAttribute("tweet", new Tweet());
+        return "newTweet";
+    }
+    
+    @PostMapping(value = "/tweets")
+    public String submitTweetForm(@Valid Tweet tweet, BindingResult bindingResult, Model model) {
+        User user = userService.getLoggedInUser();
+        if (!bindingResult.hasErrors()) {
+            tweet.setUser(user);
+            tweetService.save(tweet);
+            model.addAttribute("successMessage", "Tweet successfully created!");
+            model.addAttribute("tweet", new Tweet());
+        }
+        return "newTweet";
+    }
 
 }
