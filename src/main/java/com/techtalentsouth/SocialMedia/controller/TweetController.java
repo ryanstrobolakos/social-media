777f4e8ecb13a47b655a3f994db54d2381298e90
Ryan Strobolakos <ryanstrobolakos@Ryans-MacBook-Air.local>
2019-12-11 10:59:51 -0500

    adding query parameter to getFeed method to either show tweets of following or all tweets

--- src/main/java/com/techtalentsouth/SocialMedia/controller/TweetController.java
+++ src/main/java/com/techtalentsouth/SocialMedia/controller/TweetController.java
@@ -1,5 +1,6 @@
 package com.techtalentsouth.SocialMedia.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -11,8 +12,10 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.techtalentsouth.SocialMedia.model.Tweet;
+import com.techtalentsouth.SocialMedia.model.TweetDisplay;
 import com.techtalentsouth.SocialMedia.model.User;
 import com.techtalentsouth.SocialMedia.service.TweetService;
 import com.techtalentsouth.SocialMedia.service.UserService;
@@ -26,8 +29,23 @@ public class TweetController {
     private TweetService tweetService;
     
     @GetMapping(value= {"/tweets", "/"})
-    public String getFeed(Model model){
-        List<Tweet> tweets = tweetService.findAll();
+    public String getFeed(@RequestParam(value="filter", required=false) String filter, Model model){
+    	User loggedInUser = userService.getLoggedInUser();
+    	List<TweetDisplay> tweets = new ArrayList<>();
+    	
+    	if (filter == null) {
+    		filter = "all";
+    	}
+    	if (filter.equalsIgnoreCase("following")) {
+    		List<User> following = loggedInUser.getFollowing();
+    		tweets = tweetService.findAllByUsers(following);
+    		model.addAttribute("filter", "following");
+    	}
+    	else {
+    		tweets = tweetService.findAll();
+    		model.addAttribute("filter", "all");
+    	}
+
         model.addAttribute("tweetList", tweets);
         return "feed";
     }
@@ -52,10 +70,10 @@ public class TweetController {
     
     @GetMapping(value = "/tweets/{tag}")
     public String getTweetsByTag(@PathVariable(value="tag") String tag, Model model) {
-        List<Tweet> tweets = tweetService.findAllWithTag(tag);
-        model.addAttribute("tweetList", tweets);
-        model.addAttribute("tag", tag);
-        return "taggedTweets";
+    	List<TweetDisplay> tweets = tweetService.findAllWithTag(tag);
+    	model.addAttribute("tweetList", tweets);
+    	model.addAttribute("tag", tag);
+    	return "taggedTweets";
     }
 
 }
